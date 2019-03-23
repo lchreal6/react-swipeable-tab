01254e7ad3ecb271059e6e8d3e8ca7af589978c6
lchreal6 <lchreal6@gmail.com>
2019-03-23 11:52:50 +0800

    add hompage public path

---
+++ build/precache-manifest.690d36cd91ccfb2dc746a6666b536671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-swipeable-tab/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c2a59ac8bf9e3ecb3b5",
+    "url": "/react-swipeable-tab/build/static/js/runtime~main.dfca0ba7.js"
+  },
+  {
+    "revision": "f8743beaccb83f0a707f",
+    "url": "/react-swipeable-tab/build/static/js/main.033cf2ba.chunk.js"
+  },
+  {
+    "revision": "22cd29b3f471be2eed98",
+    "url": "/react-swipeable-tab/build/static/js/2.7f1e2e91.chunk.js"
+  },
+  {
+    "revision": "f8743beaccb83f0a707f",
+    "url": "/react-swipeable-tab/build/static/css/main.e20e63e2.chunk.css"
+  },
+  {
+    "revision": "a22c84468482828e6b053e38b917613d",
+    "url": "/react-swipeable-tab/build/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74ff1ef808f434026a09be3013fadf5b.js"
+  "/react-swipeable-tab/build/precache-manifest.690d36cd91ccfb2dc746a6666b536671.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-swipeable-tab/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
